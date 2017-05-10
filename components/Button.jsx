3499d88a7981c188a75e2b6d14f0a3178609384b
Christophe Hurpeau <christophe@hurpeau.com>
2017-05-10 23:09:56 +0200

    feat: button input

--- components/Button.jsx
+++ components/Button.jsx
@@ -9,6 +9,7 @@ type PropsType = {
   label: ?ReactNodeType,
   href: ?string,
   flat: ?boolean,
+  input: ?boolean,
   disabled: ?boolean,
 }
 
@@ -20,11 +21,13 @@ export default ({
   children,
   href,
   flat,
+  input,
   disabled,
   ...otherProps
 }: PropsType): ReactNodeType => {
-  const TagName = href ? 'a' : 'button';
-  if (TagName === 'button' && disabled) otherProps = { disabled, ...otherProps };
+  const TagName = href ? 'a' : (input ? 'input' : 'button');
+  if (TagName !== 'a' && disabled) otherProps = { disabled, ...otherProps };
+  if (TagName === 'input') otherProps = { type: 'button', ...otherProps };
   if (!label) label = children;
   return (
     <TagName
