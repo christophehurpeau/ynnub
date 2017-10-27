a37f9037970204078eae6751ac49929fa93311fa
Christophe Hurpeau <christophe@hurpeau.com>
2017-10-27 22:03:39 +0200

    fix: radio select fixes and .radio, .checkbox margin fixes

--- packages/ynnub/form/RadioSelect.jsx
+++ packages/ynnub/form/RadioSelect.jsx
@@ -1,18 +1,25 @@
 import { Children, cloneElement } from 'react';
 import Heading from '../components/Heading';
+import s from './RadioSelect.scss';
 
 export Radio from './Radio';
 
 type PropsType = {
   icon?: ?string,
   label?: ?Node,
+  prefixId: string,
 };
 
-export default ({ icon, label, children, ...props}) => {
-  children = Children.map(children, child => cloneElement(child, { ...props, ...child.props }));
+export default ({ icon, label, children, value, prefixId, ...props}) => {
+  children = Children.map(children, child => cloneElement(child, {
+    selected: value === child.props.value,
+    id: `${prefixId}-${value}`,
+    ...props,
+    ...child.props,
+  }));
 
   return (
-    <div>
+    <div className={s.container}>
       <Heading icon={icon} value={label} type="subheading2" />
       {children}
     </div>
