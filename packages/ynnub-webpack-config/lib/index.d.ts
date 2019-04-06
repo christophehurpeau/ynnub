cc239aeb3b540e5d9ca73c00e34e8c1349bc8b7f
Christophe Hurpeau <christophe@hurpeau.com>
2019-04-06 22:03:08 +0200

    fix: update .d.ts ynnub-webpack-config

--- packages/ynnub-webpack-config/lib/index.d.ts
+++ packages/ynnub-webpack-config/lib/index.d.ts
@@ -1,8 +1,19 @@
 import { Loader, RuleSetRule } from 'webpack';
 
+type Target = 'node' | 'modern-browser' | 'browser';
+
+export interface CreateCssModuleUseOptions {
+  target: Target,
+  extractLoader: Loader,
+  global?: boolean,
+  plugins: Array<any>,
+  production: boolean,
+  resolveLoader?: (loader: string) => string,
+  otherLoaders?: Loader[],
+}
 
 export interface CreateModuleRulesOptions {
-  target: 'node' | 'modern-browser' | 'browser',
+  target: Target,
   extractLoader: Loader,
   plugins: Array<any>,
   production: boolean,
@@ -19,9 +30,11 @@ export interface StylesCacheGroups {
 }
 
 
+type CreateCssModuleUseFn = (options: CreateCssModuleUseOptions) => RuleSetRule;
 type CreateModuleRulesFn = (options: CreateModuleRulesOptions) => Array<RuleSetRule>;
 
+declare const createCssModuleUse: CreateCssModuleUseFn;
 declare const createModuleRules: CreateModuleRulesFn;
 declare const stylesCacheGroups: StylesCacheGroups;
 
-export { createModuleRules, stylesCacheGroups };
+export { createCssModuleUse, createModuleRules, stylesCacheGroups };
